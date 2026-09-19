debcb4911909cb57e5fcdce59ab73b03c6f8b2ab
amatullahaloula <amatullahaloula.gmail.com>
2026-09-19 18:14:25 +0000

    Fixed the database connection

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $db_user = "naima.aloula";
-$db_pass = "";
+$db_pass = "aloula";
 $db_name = "ecommerce_2026A_naima_aloula";
 
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
